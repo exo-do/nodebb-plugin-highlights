531d2064f702e88e8ba66f4104d4410073b14aee
Fran Exposito <8macau8@gmail.com>
2015-01-06 20:09:44 +0100

    Arreglo para obtener op del post

--- library.js
+++ library.js
@@ -1,28 +1,33 @@
-"use strict";
+(function (module) {
+	"use strict";
 
-var highlights = {};
-var opUID;
-var contPost = 1;
-var User = module.parent.require('./user');
+	var highlights = {};
+	var opUID;
+	var postCount = 1;
+	var topics = module.parent.require('./topics');
 
-/**
- * Parsea todos los posts del hilo en busca de usuarios ignorados
- */
-highlights.parsePosts = function (data, callback) {
-	if (data && data.postData && data.postData.content) {
-		data.postData.isOP = false;
-		if (contPost == 1) {
-			opUID = data.postData.uid;
-			data.postData.isOP = true;
-			contPost += 1;
+	/**
+	 * Parsea todos los posts del hilo en busca de los mensajes del OP
+	 */
+	highlights.parse = function (data, callback) {
+		if (data && data.postData && data.postData.content) {
+			if (postCount == 1) {
+				topics.getTopicData(data.postData.tid, function (err, topic) {
+					if (err || !topic) {
+						return callback(err);
+					}
+					opUID = topic.uid;
+					postCount += 1;
+				});
+			}
+			data.postData.isOP = false;
+			if ( opUID == data.postData.uid ) {
+				data.postData.isOP = true;
+			}
 		}
-		else if ( opUID == data.postData.uid ) {
-			data.postData.isOP = true;
-		}
-	}
-	callback(null, data);
-};
+		callback(null, data);
+	};
 
+	module.exports = highlights;
 
-
-module.exports = highlights;
+}(module));
