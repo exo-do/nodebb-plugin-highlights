845c6fbcf16e5bdcdf2048d43612bb5d3d496f91
Fran Exposito <8macau8@gmail.com>
2015-01-07 11:02:42 +0100

    Añadida funcionalidad para seguidores

--- library.js
+++ library.js
@@ -4,28 +4,34 @@
 	var highlights = {};
 	var opUID;
 	var postCount = true;
-	var topics = module.parent.require('./topics');
+	var topics = module.parent.require('./topics'),
+	  user = module.parent.require('./user');
+
 
 	/**
 	 * Parsea todos los posts del hilo en busca de los mensajes del OP
 	 */
 	highlights.parse = function (data, callback) {
 		if (data && data.postData && data.postData.content) {
-			if (postCount) {
-				topics.getTopicData(data.postData.tid, function (err, topic) {
-					if (err || !topic) {
-						return callback(err);
-					}
-					opUID = topic.uid;
-					data.postData.isOP = false;
-					if ( opUID == data.postData.uid ) {
-						data.postData.isOP = true;
-						callback(null, data);
-					} else {
-						callback(null, data);
-					}
-				});
-			}
+			topics.getTopicData(data.postData.tid, function (err, topic) {
+				if (err || !topic) {
+					return callback(err);
+				}
+				opUID = topic.uid;
+				data.postData.isOP = false;
+				if ( opUID == data.postData.uid ) {
+					data.postData.isOP = true;
+					callback(null, data);
+				} else {
+					user.isFollowing(data.uid, data.postData.uid, function (err, isfow) {
+						if (err) {
+							return callback(err);
+						}
+						data.postData.isFollow = isfow;
+						callback(null, data)
+					});
+				}
+			});
 		} else {
 			callback(null, data);
 		}
