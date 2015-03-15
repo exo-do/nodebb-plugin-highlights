630b8906b9083d51152453af32bf2e4dfc2b6fe1
alberto__segura <alberto.segura.delgado@gmail.com>
2015-03-15 13:52:20 +0100

    changed hook to avoid problems with post cache

--- library.js
+++ library.js
@@ -5,34 +5,48 @@
 	var opUID;
 	var postCount = true;
 	var topics = module.parent.require('./topics'),
-	  user = module.parent.require('./user');
+	  user = module.parent.require('./user'),
+	  async = require('async');
 
 
 	/**
 	 * Parsea todos los posts del hilo en busca de los mensajes del OP
 	 */
 	highlights.parse = function (data, callback) {
-		if (data && data.postData && data.postData.content) {
-			topics.getTopicData(data.postData.tid, function (err, topic) {
-				if (err || !topic) {
-					return callback(err);
-				}
-				opUID = topic.uid;
-				data.postData.isOP = false;
-				if ( opUID == data.postData.uid ) {
-					data.postData.isOP = true;
-				}
-				user.isFollowing(data.uid, data.postData.uid, function (err, isfow) {
-					if (err) {
-						return callback(err);
+		async.eachSeries(data.posts,
+		function(p, cb){
+			if (p && data.uid)
+			{
+				topics.getTopicData(p.tid, function (err, topic) {
+					if (err || !topic) {
+						p.isOP = false;
+						p.isFollow = false;
+						cb();
 					}
-					data.postData.isFollow = isfow;
-					callback(null, data)
+					opUID = topic.uid;
+					p.isOP = false;
+					if ( opUID == p.uid ) {
+						p.isOP = true;
+					}
+					
+					user.isFollowing(data.uid, p.uid, function (err, isfow) {
+						if (err) {
+							cb();
+						}
+						p.isFollow = isfow;
+						console.log(p);
+						cb();
+					});
+					
 				});
-			});
-		} else {
+			}
+			else
+			{
+				cb();
+			}
+		}, function(r){
 			callback(null, data);
-		}
+		});
 	};
 
 	module.exports = highlights;
